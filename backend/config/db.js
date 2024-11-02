89778018c0112677c31a30bbfd772697ec8ea4c8
Ishan Sinha <141180090+ishan0224@users.noreply.github.com>
2024-11-03 04:14:51 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const  connectDB = async () =>{
-    await mongoose.connect('mongodb+srv://shan123:79sIPCyTOWby821t@cluster0har.gtgglkv.mongodb.net/fooddeli').then(()=>console.log("DB Connected"))
+    await mongoose.connect('mongodb+srv://<username>:<yourpassword>@cluster0har.gtgglkv.mongodb.net/fooddeli').then(()=>console.log("DB Connected"))
 }
 
 
